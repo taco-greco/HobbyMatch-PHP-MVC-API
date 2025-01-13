ddad0c74a7439add408d3440bdc2cda7e807d93f
Anastasios Statiris <tasosstatiris@outlook.fr>
2025-01-13 12:23:53 +0100

    Create User works

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,10 +2,23 @@
 
 namespace src\Controller;
 
+use src\Model\User;
+
 class UserController extends AbstractController
 {
     public function create()
     {
-        return $this->twig->render("User/create.html.twig");
+        if(isset($_POST["mail"]) && isset($_POST["password"]) && isset($_POST["roles"])){
+            $user = new User();
+            $hashpass = password_hash($_POST["password"], PASSWORD_BCRYPT, ["cost"=>12]);
+            $user->setEMail($_POST["mail"])
+            ->setPassword($hashpass)
+            ->setRoles($_POST["roles"]);
+            $id = User::SqlAdd($user);
+            header("Location:/User/login");
+            exit();
+            }else{
+            return $this->twig->render("User/create.html.twig");
+            }
     }
 }
