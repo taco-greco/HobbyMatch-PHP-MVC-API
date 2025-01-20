9ed98cd84da8b44aa910333b1a8a3b1665e3d69d
Anastasios Statiris <tasosstatiris@outlook.fr>
2025-01-20 13:42:59 +0100

    post api image

--- src/Controller/ApiHobbyController.php
+++ src/Controller/ApiHobbyController.php
@@ -37,6 +37,7 @@ class ApiHobbyController
         $data = file_get_contents("php://input");
         //Conversion du string en JSON
         $json = json_decode($data);
+
         if (empty($json)) {
             header("HTTP/1.1 400 Bad Request");
             return json_encode([
@@ -44,11 +45,43 @@ class ApiHobbyController
                 "Message" => "Il faut des données"
             ]);
         }
+
+        if (!isset($json->Titre) || !isset($json->Description)) {
+            header("HTTP/1.1 400 Bad Request");
+            return json_encode(
+                [
+                    "status" => "error",
+                    "message" => "Il faut des données"
+                ]
+            );
+        }
+
+        //Gestion de l'image
+        $sqlRepository = null;
+        $nomImage = null;
+
+        if (isset($json->Image)) {
+            $nomImage = uniqid() . ".jpg";
+            //Fabriquer le répertoire d'accueil
+            $dateNow = new \DateTime();
+            $sqlRepository = $dateNow->format('Y/m');
+            $repository = './uploads/images/' . $dateNow->format('Y/m');
+            if (!is_dir($repository)) {
+                mkdir($repository, 0777, true);
+            }
+            //Fabriquer l'image
+            $ifp = fopen($repository . "/" . $nomImage, "wb");
+            fwrite($ifp, base64_decode($json->Image));
+            fclose($ifp);
+        }
+
         $hobby = new Hobby();
         $hobby->setTitre($json->Titre)
             ->setDescription($json->Description)
             ->setDate(new \DateTime($json->DatePublication))
-            ->setAuteur($json->Auteur);
+            ->setAuteur($json->Auteur)
+            ->setImageRepository($sqlRepository)
+            ->setImageFileName($nomImage);
         $id = Hobby::SqlAdd($hobby);
         return json_encode([
             "code" => 0,
