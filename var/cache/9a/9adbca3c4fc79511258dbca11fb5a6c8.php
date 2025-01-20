28c61d00780b0ab12cf7719aa6c29ba9c866babe
Anastasios Statiris <tasosstatiris@outlook.fr>
2025-01-20 20:01:17 +0100

    Footer style done

--- var/cache/9a/9adbca3c4fc79511258dbca11fb5a6c8.php
+++ var/cache/9a/9adbca3c4fc79511258dbca11fb5a6c8.php
@@ -123,8 +123,8 @@ class __TwigTemplate_f874f5eae46d430de0fee2bb496ccdde extends Template
         yield "\t\t\t\t</main>
 \t\t\t</div>
 
-\t\t\t<div class=\"container-fluid py-4 mt-4 bg-dark\">
-\t\t\t\t<footer class=\"d-flex flex-wrap justify-content-between align-items-center border-top\">
+\t\t\t<div class=\"container-fluid py-4 mt-4 bg-dark border-top\">
+\t\t\t\t<footer class=\"d-flex flex-wrap justify-content-between align-items-center px-2 px-md-4\">
 \t\t\t\t\t<p class=\"col-md-4 mb-0 text-light\">© 2024 Company, Inc</p>
 
 \t\t\t\t\t<a href=\"/\" class=\"col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none\">
@@ -296,8 +296,8 @@ class __TwigTemplate_f874f5eae46d430de0fee2bb496ccdde extends Template
 \t\t\t\t</main>
 \t\t\t</div>
 
-\t\t\t<div class=\"container-fluid py-4 mt-4 bg-dark\">
-\t\t\t\t<footer class=\"d-flex flex-wrap justify-content-between align-items-center border-top\">
+\t\t\t<div class=\"container-fluid py-4 mt-4 bg-dark border-top\">
+\t\t\t\t<footer class=\"d-flex flex-wrap justify-content-between align-items-center px-2 px-md-4\">
 \t\t\t\t\t<p class=\"col-md-4 mb-0 text-light\">© 2024 Company, Inc</p>
 
 \t\t\t\t\t<a href=\"/\" class=\"col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none\">
