91c751cd90d8e3a95fa9d21ec8a4040ac2361945
Anastasios Statiris <tasosstatiris@outlook.fr>
2025-02-10 23:34:29 +0100

    pdf added what I forgot. Problem with adding map image

--- var/cache/bf/bf58aa7487552f4063b2e3e37161370b.php
+++ var/cache/bf/bf58aa7487552f4063b2e3e37161370b.php
@@ -39,43 +39,73 @@ class __TwigTemplate_7e0452cc51cd0b29709288991f61262a extends Template
     {
         $macros = $this->macros;
         // line 1
-        yield "<h1>";
-        yield $this->env->getRuntime('Twig\Runtime\EscaperRuntime')->escape(CoreExtension::getAttribute($this->env, $this->source, ($context["hobby"] ?? null), "titre", [], "any", false, false, false, 1), "html", null, true);
+        yield "<p>
+\t";
+        // line 2
+        if (($this->env->getFunction('file_exist')->getCallable()(((("./uploads/images/" . CoreExtension::getAttribute($this->env, $this->source,         // line 3
+($context["hobby"] ?? null), "ImageRepository", [], "any", false, false, false, 3)) . "/") . CoreExtension::getAttribute($this->env, $this->source, ($context["hobby"] ?? null), "ImageFileName", [], "any", false, false, false, 3))) && (CoreExtension::getAttribute($this->env, $this->source,         // line 4
+($context["hobby"] ?? null), "ImageFileName", [], "any", false, false, false, 4) != ""))) {
+            // line 5
+            yield "\t\t<img src=\"/uploads/images/";
+            yield $this->env->getRuntime('Twig\Runtime\EscaperRuntime')->escape(CoreExtension::getAttribute($this->env, $this->source, ($context["hobby"] ?? null), "ImageRepository", [], "any", false, false, false, 5), "html", null, true);
+            yield "/";
+            yield $this->env->getRuntime('Twig\Runtime\EscaperRuntime')->escape(CoreExtension::getAttribute($this->env, $this->source, ($context["hobby"] ?? null), "ImageFileName", [], "any", false, false, false, 5), "html", null, true);
+            // line 6
+            yield "\" class=\"img-thumbnail\"/>
+\t";
+        }
+        // line 8
+        yield "</p>
+
+<h1>";
+        // line 10
+        yield $this->env->getRuntime('Twig\Runtime\EscaperRuntime')->escape(CoreExtension::getAttribute($this->env, $this->source, ($context["hobby"] ?? null), "titre", [], "any", false, false, false, 10), "html", null, true);
         yield "</h1>
 <p>
 \t<strong>Description :</strong>
 \t";
-        // line 4
-        yield $this->env->getRuntime('Twig\Runtime\EscaperRuntime')->escape(CoreExtension::getAttribute($this->env, $this->source, ($context["hobby"] ?? null), "description", [], "any", false, false, false, 4), "html", null, true);
+        // line 13
+        yield $this->env->getRuntime('Twig\Runtime\EscaperRuntime')->escape(CoreExtension::getAttribute($this->env, $this->source, ($context["hobby"] ?? null), "description", [], "any", false, false, false, 13), "html", null, true);
         yield "</p>
 <p>
 \t<strong>Auteur :</strong>
 \t";
-        // line 7
-        yield $this->env->getRuntime('Twig\Runtime\EscaperRuntime')->escape(CoreExtension::getAttribute($this->env, $this->source, ($context["hobby"] ?? null), "Auteur", [], "any", false, false, false, 7), "html", null, true);
+        // line 16
+        yield $this->env->getRuntime('Twig\Runtime\EscaperRuntime')->escape(CoreExtension::getAttribute($this->env, $this->source, ($context["hobby"] ?? null), "Auteur", [], "any", false, false, false, 16), "html", null, true);
+        yield "</p>
+<p>
+<p>
+\t<strong>Contact :</strong>
+\t";
+        // line 20
+        yield $this->env->getRuntime('Twig\Runtime\EscaperRuntime')->escape(CoreExtension::getAttribute($this->env, $this->source, ($context["hobby"] ?? null), "EmailContact", [], "any", false, false, false, 20), "html", null, true);
+        yield "</p>
+<p>
+<p>
+\t<strong>Prix :</strong>
+\t";
+        // line 24
+        yield $this->env->getRuntime('Twig\Runtime\EscaperRuntime')->escape(CoreExtension::getAttribute($this->env, $this->source, ($context["hobby"] ?? null), "Prix", [], "any", false, false, false, 24), "html", null, true);
         yield "</p>
 <p>
 \t<strong>Date :</strong>
 \t";
-        // line 10
-        yield $this->env->getRuntime('Twig\Runtime\EscaperRuntime')->escape($this->extensions['Twig\Extension\CoreExtension']->formatDate(CoreExtension::getAttribute($this->env, $this->source, ($context["hobby"] ?? null), "DatePublication", [], "any", false, false, false, 10), "d/m/Y"), "html", null, true);
+        // line 27
+        yield $this->env->getRuntime('Twig\Runtime\EscaperRuntime')->escape($this->extensions['Twig\Extension\CoreExtension']->formatDate(CoreExtension::getAttribute($this->env, $this->source, ($context["hobby"] ?? null), "DatePublication", [], "any", false, false, false, 27), "d/m/Y"), "html", null, true);
         yield "</p>
 <p>
+<p>
+\t<strong>Latitude :</strong>
 \t";
-        // line 12
-        if (($this->env->getFunction('file_exist')->getCallable()(((("./uploads/images/" . CoreExtension::getAttribute($this->env, $this->source,         // line 13
-($context["hobby"] ?? null), "ImageRepository", [], "any", false, false, false, 13)) . "/") . CoreExtension::getAttribute($this->env, $this->source, ($context["hobby"] ?? null), "ImageFileName", [], "any", false, false, false, 13))) && (CoreExtension::getAttribute($this->env, $this->source,         // line 14
-($context["hobby"] ?? null), "ImageFileName", [], "any", false, false, false, 14) != ""))) {
-            // line 15
-            yield "\t\t<img src=\"/uploads/images/";
-            yield $this->env->getRuntime('Twig\Runtime\EscaperRuntime')->escape(CoreExtension::getAttribute($this->env, $this->source, ($context["hobby"] ?? null), "ImageRepository", [], "any", false, false, false, 15), "html", null, true);
-            yield "/";
-            yield $this->env->getRuntime('Twig\Runtime\EscaperRuntime')->escape(CoreExtension::getAttribute($this->env, $this->source, ($context["hobby"] ?? null), "ImageFileName", [], "any", false, false, false, 15), "html", null, true);
-            // line 16
-            yield "\" class=\"img-thumbnail\"/>
+        // line 31
+        yield $this->env->getRuntime('Twig\Runtime\EscaperRuntime')->escape(CoreExtension::getAttribute($this->env, $this->source, ($context["hobby"] ?? null), "Latitude", [], "any", false, false, false, 31), "html", null, true);
+        yield "</p>
+<p>
+<p>
+\t<strong>Longitude :</strong>
 \t";
-        }
-        // line 18
+        // line 35
+        yield $this->env->getRuntime('Twig\Runtime\EscaperRuntime')->escape(CoreExtension::getAttribute($this->env, $this->source, ($context["hobby"] ?? null), "Longitude", [], "any", false, false, false, 35), "html", null, true);
         yield "</p>
 ";
         yield from [];
@@ -102,22 +132,12 @@ class __TwigTemplate_7e0452cc51cd0b29709288991f61262a extends Template
      */
     public function getDebugInfo(): array
     {
-        return array (  79 => 18,  75 => 16,  70 => 15,  68 => 14,  67 => 13,  66 => 12,  61 => 10,  55 => 7,  49 => 4,  42 => 1,);
+        return array (  108 => 35,  101 => 31,  94 => 27,  88 => 24,  81 => 20,  74 => 16,  68 => 13,  62 => 10,  58 => 8,  54 => 6,  49 => 5,  47 => 4,  46 => 3,  45 => 2,  42 => 1,);
     }
 
     public function getSourceContext(): Source
     {
-        return new Source("<h1>{{ hobby.titre }}</h1>
-<p>
-\t<strong>Description :</strong>
-\t{{ hobby.description }}</p>
-<p>
-\t<strong>Auteur :</strong>
-\t{{ hobby.Auteur }}</p>
-<p>
-\t<strong>Date :</strong>
-\t{{ hobby.DatePublication|date(\"d/m/Y\") }}</p>
-<p>
+        return new Source("<p>
 \t{% if file_exist(
 './uploads/images/'~hobby.ImageRepository~'/'~hobby.ImageFileName ) and
 hobby.ImageFileName !=\"\" %}
@@ -125,6 +145,33 @@ hobby.ImageFileName !=\"\" %}
 \t\t\t}}\" class=\"img-thumbnail\"/>
 \t{% endif %}
 </p>
+
+<h1>{{ hobby.titre }}</h1>
+<p>
+\t<strong>Description :</strong>
+\t{{ hobby.description }}</p>
+<p>
+\t<strong>Auteur :</strong>
+\t{{ hobby.Auteur }}</p>
+<p>
+<p>
+\t<strong>Contact :</strong>
+\t{{ hobby.EmailContact }}</p>
+<p>
+<p>
+\t<strong>Prix :</strong>
+\t{{ hobby.Prix }}</p>
+<p>
+\t<strong>Date :</strong>
+\t{{ hobby.DatePublication|date(\"d/m/Y\") }}</p>
+<p>
+<p>
+\t<strong>Latitude :</strong>
+\t{{ hobby.Latitude }}</p>
+<p>
+<p>
+\t<strong>Longitude :</strong>
+\t{{ hobby.Longitude }}</p>
 ", "Hobby/pdf.html.twig", "C:\\wamp64\\www\\hobbymatch\\src\\View\\Hobby\\pdf.html.twig");
     }
 }
